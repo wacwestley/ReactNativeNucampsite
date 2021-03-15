b453f121d44b7c75cfa1ba50f1f1ff9b30c7e0eb
WestleyCer <wacwestley@gmail.com>
2021-03-15 04:18:22 -0400

    Swipe and Delete Favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -158,9 +158,10 @@ export const postComment = (campsiteId, rating, author, text) => dispatch => {
         campsiteId,
         rating,
         author,
-        text,
-        date: new Date().toISOString()
+        text
     };
+    newComment.date = new Date().toISOString();
+
     setTimeout(() => {
         dispatch(addComment(newComment));
     }, 2000);
@@ -169,4 +170,9 @@ export const postComment = (campsiteId, rating, author, text) => dispatch => {
 export const addComment = comment => ({
     type: ActionTypes.ADD_COMMENT,
     payload: comment
+});
+
+export const deleteFavorite = campsiteId => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: campsiteId
 });
